3981f2bf6acf17f7a74d4a8986bd762f4f3a74eb
pham  van hieu <hieupvd00585@fpt.edu.vn>
2020-04-26 23:12:29 +0700

    complete

---
+++ index.js
@@ -0,0 +1,20 @@
+const {ApolloServer,PubSub} = require('apollo-server');
+
+const mongoose = require('mongoose');
+const {MONGODB }= require('./config.js')
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
+const pubsub =  new PubSub();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({req})=>({req,pubsub})
+});
+mongoose.connect(MONGODB,{useNewUrlParser:true})
+.then(()=>{
+    console.log('MongDB Connected');
+    return server.listen({port:5000});
+})
+.then((res)=>{
+    console.log(`Server  running at ${res.url}`)
+})
